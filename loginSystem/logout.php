cf4b530e47d222af5ba0265469b9d2188b7ab714
MayankSalvi <mayanksalvi.8@gmail.com>
2022-11-08 17:21:56 +0530

    commit

--- loginSystem/logout.php
+++ loginSystem/logout.php
@@ -4,5 +4,7 @@ session_start();
 session_destroy();
 session_unset($_SESSION['id']);
 session_unset($_SESSION['fullname']);
+setcookie('emailCookie', '', time() - 86400);
+setcookie('passwordCookie', '', time() - 86400);
 header("Location:login.php");
 echo '1';
